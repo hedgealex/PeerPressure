17476d541705b66c7ebf6b76e59f9b9572c35dbc
ddk444 <ddkahn@umich.edu>
2018-11-06 15:08:57 -0500

    Creating new Login Activity (Activity #1)

---
+++ app/src/main/java/com/example/alexhedge/peerpressure/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.alexhedge.peerpressure;
+
+//This is the first (1st) activity 
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
