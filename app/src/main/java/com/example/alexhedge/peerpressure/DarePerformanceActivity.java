1363be7ba5e22fafa5c319a56914c5cde342cdf7
Zach <zagerndt@umich.edu>
2018-11-08 16:49:16 -0500

    Zach Add activities 4-9

---
+++ app/src/main/java/com/example/alexhedge/peerpressure/DarePerformanceActivity.java
@@ -0,0 +1,13 @@
+package com.example.alexhedge.peerpressure;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DarePerformanceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dare_performance);
+    }
+}
